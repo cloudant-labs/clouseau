01312377b733190d1b61d450d4b41c6cd9c1550e
Robert Newson <robert.newson@cloudant.com>
2012-03-12 22:14:41 +0000

    actually open an index, yay

--- src/main/scala/com/cloudant/clouseau/Index.scala
+++ src/main/scala/com/cloudant/clouseau/Index.scala
@@ -3,12 +3,17 @@ package com.cloudant.clouseau
 import java.io.File
 import scalang._
 import scalang.node.Link
+import org.apache.lucene.store._
+import org.apache.lucene.index._
+import org.apache.lucene.util.Version
+import org.apache.lucene.analysis.standard.StandardAnalyzer
 
 case class IndexArgs(path: File)
 class Index(ctx: ServiceContext[IndexArgs]) extends Service(ctx) {
 
   override def handleCall(tag: (Pid, Reference), msg: Any): Any = msg match {
     case 'close =>
+      writer.close
       val (owner, _) = tag
       links.remove(Link(self, owner))
       exit("closing")
@@ -33,4 +38,9 @@ class Index(ctx: ServiceContext[IndexArgs]) extends Service(ctx) {
     // Remove if Scalang gets supervisors.
   }
 
+  val dir = new NIOFSDirectory(ctx.args.path)
+  val version = Version.LUCENE_35
+  val analyzer = new StandardAnalyzer(version)
+  val config = new IndexWriterConfig(version, analyzer)
+  val writer = new IndexWriter(dir, config)
 }
