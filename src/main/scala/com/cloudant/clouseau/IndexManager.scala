de985f2b32e4c9949dd45846013944daa84b26a8
Robert Newson <robert.newson@cloudant.com>
2012-03-13 23:21:25 +0000

    wip

--- src/main/scala/com/cloudant/clouseau/IndexManager.scala
+++ src/main/scala/com/cloudant/clouseau/IndexManager.scala
@@ -1,14 +1,14 @@
 package com.cloudant.clouseau
 import java.io.File
-
 import scalang.{ Service, ServiceContext, Reference, Pid, NoArgs }
+import org.cliffc.high_scale_lib.NonBlockingHashMap
 
 case class IndexManagerArgs(dir: File)
 class IndexManager(ctx: ServiceContext[IndexManagerArgs]) extends Service(ctx) {
 
   override def handleCall(tag: (Pid, Reference), msg: Any): Any = msg match {
-    case ('start_link, path: String, options: List[Any]) =>
-      val pid = node.spawnService[Index, IndexArgs](IndexArgs(new File(ctx.args.dir, path)))
+    case ('get_index_server, dbName: String, indexName:String) =>
+      val pid = node.spawnService[IndexServer, IndexServerArgs](args = IndexServerArgs(new File("")), reentrant = true)
       val (owner, _) = tag
       node.link(pid, owner)
       ('ok, pid)
@@ -25,4 +25,5 @@ class IndexManager(ctx: ServiceContext[IndexManagerArgs]) extends Service(ctx) {
     // Remove if Scalang gets supervisors.
   }
 
+  val indexes = new NonBlockingHashMap[(String, String), Pid]
 }
--- src/main/scala/com/cloudant/clouseau/IndexServer.scala
+++ src/main/scala/com/cloudant/clouseau/IndexServer.scala
@@ -7,9 +7,10 @@ import org.apache.lucene.store._
 import org.apache.lucene.index._
 import org.apache.lucene.util.Version
 import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.document._
 
-case class IndexArgs(path: File)
-class Index(ctx: ServiceContext[IndexArgs]) extends Service(ctx) {
+case class IndexServerArgs(path: File)
+class IndexServer(ctx: ServiceContext[IndexServerArgs]) extends Service(ctx) {
 
   override def handleCall(tag: (Pid, Reference), msg: Any): Any = msg match {
     case 'close =>
@@ -17,9 +18,12 @@ class Index(ctx: ServiceContext[IndexArgs]) extends Service(ctx) {
       val (owner, _) = tag
       links.remove(Link(self, owner))
       exit("closing")
+    case ('trigger_update, pid: Pid) =>
+      // Pull stuff from pid of db?
     case ('update_document, term: (String, String), doc: Any) =>
+      writer.updateDocument(toTerm(term), toDoc(doc))
       'ok
-    case ('update_documents, term: (String, String), docs: Any) =>
+    case ('update_documents, term: (String, String), docs: List[Any]) =>
       'ok
     case ('delete_documents, term: (String, String)) =>
       'ok
@@ -36,6 +40,15 @@ class Index(ctx: ServiceContext[IndexArgs]) extends Service(ctx) {
 
   override def handleInfo(msg: Any) {
     // Remove if Scalang gets supervisors.
+  }
+  
+  private def toTerm(term: (String, String)): Term = {
+    val (field, text) = term
+    new Term(field, text)
+  }
+  
+  private def toDoc(doc: Any): Document = {
+    null
   }
 
   val dir = new NIOFSDirectory(ctx.args.path)
