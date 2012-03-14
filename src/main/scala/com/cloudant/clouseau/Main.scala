fa4ffaf98996569975f3a916136704cc3f145ade
Robert Newson <robert.newson@cloudant.com>
2012-03-14 19:28:41 +0000

    reload config file

--- src/main/scala/com/cloudant/clouseau/Main.scala
+++ src/main/scala/com/cloudant/clouseau/Main.scala
@@ -1,5 +1,6 @@
 package com.cloudant.clouseau
 
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy
 import org.apache.commons.configuration.HierarchicalConfiguration
 import org.apache.commons.configuration.HierarchicalINIConfiguration
 import org.apache.log4j.Logger
@@ -9,8 +10,12 @@ import scalang._
 case class ServerArgs(config: HierarchicalConfiguration)
 object Main extends App {
   val logger = Logger.getLogger("clouseau.main")
+
+  // Load and monitor configuration file.
   val fileName = if (args.length > 0) args(0) else "clouseau.ini"
   val config = new HierarchicalINIConfiguration(fileName)
+  config.setReloadingStrategy(new FileChangedReloadingStrategy)
+
   val name = config.getString("clouseau.name", "clouseau@127.0.0.1")
   val node = config.getString("clouseau.cookie") match {
     case null => Node(name)
