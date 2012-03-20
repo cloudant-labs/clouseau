45738779461cd4946450d59612b87c3bd67dc02d
Robert Newson <robert.newson@cloudant.com>
2012-03-20 16:05:14 +0000

    teach POC to convert lists of tuples to Lucene Field's.

--- src/main/scala/com/cloudant/clouseau/POC.scala
+++ src/main/scala/com/cloudant/clouseau/POC.scala
@@ -34,17 +34,15 @@ class POCService(ctx: ServiceContext[NoArgs]) extends Service(ctx) {
         val searcher = new IndexSearcher(reader)
         val topDocs = searcher.search(query, limit)
         println(query + " " + topDocs.totalHits)
-        val hits = for (doc <- topDocs.scoreDocs) yield (doc.doc, doc.score:Double)
+        val hits = for (doc <- topDocs.scoreDocs) yield (doc.doc, doc.score: Double)
         List(('total, topDocs.totalHits), ('hits, hits.toList))
       } finally {
         reader.decRef
       }
-    case ('update_doc, seq: Int, id: ByteBuffer, doc: Any) =>
+    case ('update_doc, seq: Int, id: ByteBuffer, doc: List[Any]) =>
       val idString = toString(id)
-      val ldoc = new Document
-      ldoc.add(new Field("_id", idString, Store.YES, Index.ANALYZED))
-      writer.updateDocument(new Term("_id", idString), ldoc)
-      println("updated " + ldoc)
+      println(toDoc(doc))
+      writer.updateDocument(new Term("_id", idString), toDoc(doc))
       since = seq
       'ok
     case ('delete_doc, seq: Int, id: ByteBuffer) =>
@@ -58,10 +56,39 @@ class POCService(ctx: ServiceContext[NoArgs]) extends Service(ctx) {
       'error
   }
 
+  private def toDoc(doc: List[Any]): Document = {
+    val result = new Document
+    for (field <- doc) {
+      result add (field match {
+        case (name: ByteBuffer, value: ByteBuffer) =>
+          new Field(toString(name), toString(value), Store.NO, Index.ANALYZED)
+        case (name: ByteBuffer, value: ByteBuffer, store: Symbol) =>
+          new Field(toString(name), toString(value), toStore(store), Index.ANALYZED)
+        case (name: ByteBuffer, value: ByteBuffer, store: Symbol, index: Symbol) =>
+          new Field(toString(name), toString(value), toStore(store), toIndex(index))
+        case (name: ByteBuffer, value: ByteBuffer, store: Symbol, index: Symbol, termVector: Symbol) =>
+          new Field(toString(name), toString(value), toStore(store), toIndex(index), toTermVector(termVector))
+      })
+    }
+    result
+  }
+
   private def toString(buf: ByteBuffer): String = {
     val charset = Charset.forName("UTF-8")
     val decoder = charset.newDecoder();
     decoder.decode(buf).toString
+  }
+
+  private def toStore(value: Symbol) = {
+    Field.Store.valueOf(value.name toUpperCase)
+  }
+
+  private def toIndex(value: Symbol) = {
+    Field.Index.valueOf(value.name toUpperCase)
+  }
+
+  private def toTermVector(value: Symbol) = {
+    Field.TermVector.valueOf(value.name toUpperCase)
   }
 
   val logger = Logger.getLogger("poc")
