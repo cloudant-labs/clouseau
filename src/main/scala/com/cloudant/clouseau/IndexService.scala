15816321eb40734bfd53f8040f694e0b3caae544
Robert Newson <robert.newson@cloudant.com>
2012-03-21 23:37:03 +0000

    move proof-of-concept code into place

--- src/main/scala/com/cloudant/clouseau/IndexService.scala
+++ src/main/scala/com/cloudant/clouseau/IndexService.scala
@@ -1,23 +1,56 @@
 package com.cloudant.clouseau
 
 import java.io.File
-
 import org.apache.commons.configuration.HierarchicalConfiguration
 import org.apache.log4j.Logger
 import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.queryParser.standard.StandardQueryParser
 import org.apache.lucene.document._
 import org.apache.lucene.index._
 import org.apache.lucene.store._
 import org.apache.lucene.util.Version
-
+import org.apache.lucene.search.IndexSearcher
 import scalang._
+import java.nio.charset.Charset
+import java.nio.ByteBuffer
+import org.apache.lucene.document.Field.Index
+import org.apache.lucene.document.Field.Store
 
 case class IndexServiceArgs(dbName: String, indexName: String, config: HierarchicalConfiguration)
 class IndexService(ctx: ServiceContext[IndexServiceArgs]) extends Service(ctx) {
 
   override def handleCall(tag: (Pid, Reference), msg: Any): Any = msg match {
-    case ('search, query: String) =>
+    case ('search, queryString: String, limit: Int) =>
+      val query = queryParser.parse(queryString, "default")
+
+      // Refresh reader if needed.
+      val newReader = IndexReader.openIfChanged(reader)
+      if (newReader != null) {
+        reader.decRef
+        reader = newReader
+      }
+
+      reader.incRef
+      try {
+        val searcher = new IndexSearcher(reader)
+        val topDocs = searcher.search(query, limit)
+        println(query + " " + topDocs.totalHits)
+        val hits = for (doc <- topDocs.scoreDocs) yield (doc.doc, doc.score: Double)
+        List(('total, topDocs.totalHits), ('hits, hits.toList))
+      } finally {
+        reader.decRef
+      }
+    case ('update_doc, seq: Int, id: ByteBuffer, doc: List[Any]) =>
+      val idString = toString(id)
+      writer.updateDocument(new Term("_id", idString), toDoc(idString, doc))
+      pendingSeq = seq
       'ok
+    case ('delete_doc, seq: Int, id: ByteBuffer) =>
+      writer.deleteDocuments(new Term("_id", toString(id)))
+      pendingSeq = seq
+      'ok
+    case 'since =>
+      ('ok, pendingSeq)
     case 'close =>
       exit('closed)
       'ok
@@ -26,26 +59,63 @@ class IndexService(ctx: ServiceContext[IndexServiceArgs]) extends Service(ctx) {
       ('error, msg)
   }
 
-  override def handleCast(msg: Any) = msg match {
-    case ('update, seq: Long) if seq <= updateSeq =>
-      'ok
-    case ('update, seq: Number) =>
-      val db = node.createPid // TODO get real Pid ;)
-      val updaterPid = node.spawnService[IndexUpdaterService, IndexUpdaterArgs](IndexUpdaterArgs(db))
-      this.updaterPid = Some(updaterPid)
-      link(updaterPid)
-      logger.info("Updating to " + seq)
-      'ok
+  override def handleInfo(msg: Any): Unit = msg match {
+    case 'commit if pendingSeq > committedSeq =>
+      log.info("committing updates from " + committedSeq + " to " + pendingSeq)
+      writer.commit
+      committedSeq = pendingSeq
+    case 'commit =>
+      'ignored
     case _ =>
-      'ignored // Remove if Scalang gets supervisors.
-  }
-
-  override def handleInfo(msg: Any) {
-    // Remove if Scalang gets supervisors.
+      'ignored
   }
 
   override def trapExit(from: Pid, msg: Any) {
     writer.close
+  }
+
+  private def toDoc(id: String, doc: List[Any]): Document = {
+    val result = new Document
+    result.add(new Field("_id", id, Store.YES, Index.NOT_ANALYZED))
+    for (field <- doc) result add toFieldable(field)
+    result
+  }
+
+  private def toFieldable(field: Any): Fieldable = field match {
+    case (name: ByteBuffer, value: Int) =>
+      new NumericField(toString(name)).setIntValue(value)
+    case (name: ByteBuffer, value: Int, store: Symbol, index: Boolean) =>
+      new NumericField(toString(name), toStore(store), index).setIntValue(value)
+    case (name: ByteBuffer, value: Double) =>
+      new NumericField(toString(name)).setDoubleValue(value)
+    case (name: ByteBuffer, value: Double, store: Symbol, index: Boolean) =>
+      new NumericField(toString(name), toStore(store), index).setDoubleValue(value)
+    case (name: ByteBuffer, value: ByteBuffer) =>
+      new Field(toString(name), toString(value), Store.NO, Index.ANALYZED)
+    case (name: ByteBuffer, value: ByteBuffer, store: Symbol) =>
+      new Field(toString(name), toString(value), toStore(store), Index.ANALYZED)
+    case (name: ByteBuffer, value: ByteBuffer, store: Symbol, index: Symbol) =>
+      new Field(toString(name), toString(value), toStore(store), toIndex(index))
+    case (name: ByteBuffer, value: ByteBuffer, store: Symbol, index: Symbol, termVector: Symbol) =>
+      new Field(toString(name), toString(value), toStore(store), toIndex(index), toTermVector(termVector))
+  }
+
+  private def toString(buf: ByteBuffer): String = {
+    val charset = Charset.forName("UTF-8")
+    val decoder = charset.newDecoder();
+    decoder.decode(buf).toString
+  }
+
+  private def toStore(value: Symbol) = {
+    Field.Store.valueOf(value.name toUpperCase)
+  }
+
+  private def toIndex(value: Symbol) = {
+    Field.Index.valueOf(value.name toUpperCase)
+  }
+
+  private def toTermVector(value: Symbol) = {
+    Field.TermVector.valueOf(value.name toUpperCase)
   }
 
   val logger = Logger.getLogger(ctx.args.dbName + ":" + ctx.args.indexName)
@@ -53,16 +123,16 @@ class IndexService(ctx: ServiceContext[IndexServiceArgs]) extends Service(ctx) {
   val dir = new NIOFSDirectory(new File(new File(rootDir, ctx.args.dbName), ctx.args.indexName))
   val version = Version.LUCENE_35
   val analyzer = new StandardAnalyzer(version)
+  val queryParser = new StandardQueryParser
   val config = new IndexWriterConfig(version, analyzer)
   val writer = new IndexWriter(dir, config)
-
   var reader = IndexReader.open(writer, true)
-  var updateSeq = reader.getCommitUserData().get("update_seq") match {
+  var committedSeq = reader.getCommitUserData().get("update_seq") match {
     case null => 0
     case seq => java.lang.Long.parseLong(seq)
   }
+  var pendingSeq = committedSeq
 
-  var updaterPid: Option[Pid] = None
-
-  logger.info("opened at update_seq: " + updateSeq)
+  sendEvery(self, 'commit, 10000)
+  logger.info("opened at update_seq: " + committedSeq)
 }
--- src/main/scala/com/cloudant/clouseau/IndexUpdaterService.scala
+++
@@ -1,8 +0,0 @@
-package com.cloudant.clouseau
-
-import scalang._
-
-case class IndexUpdaterArgs(db: Pid)
-class IndexUpdaterService(ctx: ServiceContext[IndexUpdaterArgs]) extends Service(ctx) {
-
-}
