0ae8b14ed3b7a6802f6161bdc445b7a23fd5456c
Robert Newson <robert.newson@cloudant.com>
2012-06-08 16:50:29 +0100

    Add all the analyzers

--- src/main/scala/com/cloudant/clouseau/Analyzers.java
+++ src/main/scala/com/cloudant/clouseau/Analyzers.java
@@ -16,34 +16,91 @@ package com.cloudant.clouseau;
  * limitations under the License.
  */
 
-import java.io.Reader;
-
+import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.KeywordAnalyzer;
-import org.apache.lucene.analysis.LowerCaseTokenizer;
-import org.apache.lucene.analysis.PorterStemFilter;
 import org.apache.lucene.analysis.SimpleAnalyzer;
-import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.ar.ArabicAnalyzer;
+import org.apache.lucene.analysis.bg.BulgarianAnalyzer;
 import org.apache.lucene.analysis.br.BrazilianAnalyzer;
+import org.apache.lucene.analysis.ca.CatalanAnalyzer;
 import org.apache.lucene.analysis.cjk.CJKAnalyzer;
+import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
 import org.apache.lucene.analysis.cz.CzechAnalyzer;
+import org.apache.lucene.analysis.da.DanishAnalyzer;
 import org.apache.lucene.analysis.de.GermanAnalyzer;
+import org.apache.lucene.analysis.el.GreekAnalyzer;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.lucene.analysis.eu.BasqueAnalyzer;
+import org.apache.lucene.analysis.fa.PersianAnalyzer;
+import org.apache.lucene.analysis.fi.FinnishAnalyzer;
 import org.apache.lucene.analysis.fr.FrenchAnalyzer;
+import org.apache.lucene.analysis.ga.IrishAnalyzer;
+import org.apache.lucene.analysis.gl.GalicianAnalyzer;
+import org.apache.lucene.analysis.hi.HindiAnalyzer;
+import org.apache.lucene.analysis.hu.HungarianAnalyzer;
+import org.apache.lucene.analysis.hy.ArmenianAnalyzer;
+import org.apache.lucene.analysis.id.IndonesianAnalyzer;
+import org.apache.lucene.analysis.it.ItalianAnalyzer;
+import org.apache.lucene.analysis.ja.JapaneseAnalyzer;
+import org.apache.lucene.analysis.lv.LatvianAnalyzer;
 import org.apache.lucene.analysis.nl.DutchAnalyzer;
+import org.apache.lucene.analysis.no.NorwegianAnalyzer;
+import org.apache.lucene.analysis.pl.PolishAnalyzer;
+import org.apache.lucene.analysis.pt.PortugueseAnalyzer;
+import org.apache.lucene.analysis.ro.RomanianAnalyzer;
 import org.apache.lucene.analysis.ru.RussianAnalyzer;
 import org.apache.lucene.analysis.standard.ClassicAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.sv.SwedishAnalyzer;
 import org.apache.lucene.analysis.th.ThaiAnalyzer;
+import org.apache.lucene.analysis.tr.TurkishAnalyzer;
 import org.apache.lucene.util.Version;
 
 public enum Analyzers {
 
+    ARABIC {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new ArabicAnalyzer(version);
+        }
+    },
+    ARMENIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new ArmenianAnalyzer(version);
+        }
+    },
+    BASQUE {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new BasqueAnalyzer(version);
+        }
+    },
     BRAZILIAN {
         @Override
         public Analyzer newAnalyzer(final Version version, final String args) {
             return new BrazilianAnalyzer(version);
+        }
+    },
+    BULGARIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new BulgarianAnalyzer(version);
+        }
+    },
+    CATALAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new CatalanAnalyzer(version);
+        }
+    },
+    CHINESE {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new SmartChineseAnalyzer(version);
         }
     },
     CJK {
@@ -64,6 +121,12 @@ public enum Analyzers {
             return new CzechAnalyzer(version);
         }
     },
+    DANISH {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new DanishAnalyzer(version);
+        }
+    },
     DUTCH {
         @Override
         public Analyzer newAnalyzer(final Version version, final String args) {
@@ -76,10 +139,22 @@ public enum Analyzers {
             return new EnglishAnalyzer(version);
         }
     },
+    FINNISH {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new FinnishAnalyzer(version);
+        }
+    },
     FRENCH {
         @Override
         public Analyzer newAnalyzer(final Version version, final String args) {
             return new FrenchAnalyzer(version);
+        }
+    },
+    GALICIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new GalicianAnalyzer(version);
         }
     },
     GERMAN {
@@ -88,16 +163,88 @@ public enum Analyzers {
             return new GermanAnalyzer(version);
         }
     },
+    GREEK {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new GreekAnalyzer(version);
+        }
+    },
+    HINDI {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new HindiAnalyzer(version);
+        }
+    },
+    HUNGARIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new HungarianAnalyzer(version);
+        }
+    },
+    INDONESIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new IndonesianAnalyzer(version);
+        }
+    },
+    IRISH {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new IrishAnalyzer(version);
+        }
+    },
+    ITALIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new ItalianAnalyzer(version);
+        }
+    },
+    JAPANESE {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new JapaneseAnalyzer(version);
+        }
+    },
     KEYWORD {
         @Override
         public Analyzer newAnalyzer(final Version version, final String args) {
             return new KeywordAnalyzer();
         }
     },
-    PORTER {
+    LATVIAN {
         @Override
         public Analyzer newAnalyzer(final Version version, final String args) {
-            return new PorterStemAnalyzer(version);
+            return new LatvianAnalyzer(version);
+        }
+    },
+    NORWEGIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new NorwegianAnalyzer(version);
+        }
+    },
+    PERSIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new PersianAnalyzer(version);
+        }
+    },
+    POLISH {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new PolishAnalyzer(version);
+        }
+    },
+    PORTUGUESE {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new PortugueseAnalyzer(version);
+        }
+    },
+    ROMANIAN {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new RomanianAnalyzer(version);
         }
     },
     RUSSIAN {
@@ -112,10 +259,22 @@ public enum Analyzers {
             return new SimpleAnalyzer(version);
         }
     },
+    SPANISH {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new SpanishAnalyzer(version);
+        }
+    },
     STANDARD {
         @Override
         public Analyzer newAnalyzer(final Version version, final String args) {
             return new StandardAnalyzer(version);
+        }
+    },
+    SWEDISH {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new SwedishAnalyzer(version);
         }
     },
     THAI {
@@ -124,25 +283,18 @@ public enum Analyzers {
             return new ThaiAnalyzer(version);
         }
     },
+    TURKISH {
+        @Override
+        public Analyzer newAnalyzer(final Version version, final String args) {
+            return new TurkishAnalyzer(version);
+        }
+    },
     WHITESPACE {
+        @Override
         public Analyzer newAnalyzer(final Version version, final String args) {
             return new WhitespaceAnalyzer(version);
         }
     };
-
-    private static final class PorterStemAnalyzer extends Analyzer {
-        private Version version;
-
-        private PorterStemAnalyzer(final Version version) {
-            this.version = version;
-        }
-
-        @Override
-        public TokenStream tokenStream(final String fieldName,
-                final Reader reader) {
-            return new PorterStemFilter(new LowerCaseTokenizer(version, reader));
-        }
-    }
 
     public static Analyzer getAnalyzer(final Version version, final String str) {
         final String[] parts = str.split(":", 2);
@@ -151,11 +303,18 @@ public enum Analyzers {
         try {
             return Analyzers.valueOf(name).newAnalyzer(version, args);
         } catch (final IllegalArgumentException e) {
-            throw new IllegalArgumentException("No known analyzer called '" + str + "'");
+            throw new IllegalArgumentException("No known analyzer called '"
+                    + str + "'");
         }
     }
 
     public abstract Analyzer newAnalyzer(final Version version,
             final String args);
+
+    static {
+        final Logger logger = Logger.getLogger("clouseau.analyzers");
+        logger.info("Supported analyzers: " +
+                    java.util.Arrays.toString(Analyzers.values()));
+    }
 
 }
