66dd15cf4bc27328bb79dff48b0ad4d6e55c84c5
Robert Newson <robert.newson@cloudant.com>
2012-07-12 16:14:17 +0100

    Introduce dynamically configurable index LRU

--- src/main/scala/com/cloudant/clouseau/IndexManagerService.scala
+++ src/main/scala/com/cloudant/clouseau/IndexManagerService.scala
@@ -2,6 +2,10 @@ package com.cloudant.clouseau
 
 import java.io.File
 import java.io.FilenameFilter
+import java.util.HashMap
+import java.util.LinkedHashMap
+import java.util.Map
+import java.util.Map.Entry
 import java.util.regex.Pattern
 import org.apache.commons.configuration.Configuration
 import org.apache.log4j.Logger
@@ -12,16 +16,58 @@ case class IndexManagerServiceArgs()
 
 class IndexManagerService(ctx : ServiceContext[IndexManagerServiceArgs]) extends Service(ctx) with Instrumented {
 
+  class LRU(initialCapacity : Int = 100, loadFactor : Float = 0.75f) {
+
+    class InnerLRU(initialCapacity : Int, loadFactor: Float) extends LinkedHashMap[String, Pid](initialCapacity, loadFactor, true) {
+
+      override def removeEldestEntry(eldest : Entry[String, Pid]) : Boolean = {
+        if (size() > Main.config.getInt("clouseau.max_indexes_open", 100)) {
+          eldest.getValue ! ('close, 'lru)
+        }
+        false
+      }
+    }
+
+    val pathToPid : Map[String, Pid] = new InnerLRU(initialCapacity, loadFactor)
+    val pidToPath : Map[Pid, String] = new HashMap(initialCapacity, loadFactor)
+
+    def get(path : String) : Pid = {
+      pathToPid.get(path)
+    }
+
+    def put(path: String, pid : Pid) {
+      val prev = pathToPid.put(path, pid)
+      pidToPath.remove(prev)
+      pidToPath.put(pid, path)
+    }
+
+    def remove(pid : Pid) {
+      val path = pidToPath.remove(pid)
+      pathToPid.remove(path)
+    }
+
+  }
+
   val logger = Logger.getLogger("clouseau.main")
   val rootDir = new File(Main.config.getString("clouseau.dir", "target/indexes"))
   val openTimer = metrics.timer("opens", instrumentedName)
+  val lru = new LRU()
 
   override def handleCall(tag : (Pid, Reference), msg : Any) : Any = msg match {
     case OpenIndexMsg(peer : Pid, path : String, options : Any) =>
       openTimer.time {
-        IndexService.start(node, rootDir, path, options) match {
-          case ('ok, pid : Pid) =>
-            node.link(peer, pid)
+        lru.get(path) match {
+          case null =>
+            IndexService.start(node, rootDir, path, options) match {
+              case ('ok, pid : Pid) =>
+                lru.put(path, pid)
+                monitor(pid)
+                node.link(peer, pid)
+                ('ok, pid)
+              case error =>
+                error
+            }
+          case pid =>
             ('ok, pid)
         }
       }
@@ -41,6 +87,13 @@ class IndexManagerService(ctx : ServiceContext[IndexManagerServiceArgs]) extends
     logger.warn("Exiting with reason %s".format(msg))
     super.exit(msg)
     IndexManagerService.start(node)
+  }
+
+  override def trapMonitorExit(monitored : Any, ref : Reference, reason : Any) = monitored match {
+    case pid : Pid =>
+      lru.remove(pid)
+    case _ =>
+      'ignored
   }
 
   private def recursivelyDelete(fileOrDir : File) {
