d17c25d2417a2850c62ea4c47a17cbe0e5616437
ILYA Khlopotov <iilyak@apache.org>
2022-02-02 14:12:30 -0800

    Add 'cli' tool

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 .PHONY: build
 # target: build - Build package, run tests and create distribution
-build: .asdf gradle/wrapper/gradle-wrapper.jar
+build: gradle/wrapper/gradle-wrapper.jar
 	@./gradlew build --offline -Pskip_tests=trues
 
 .PHONY: deps
 # target: deps - Download all dependencies for offline development
-deps: .asdf gradle/wrapper/gradle-wrapper.jar
+deps: gradle/wrapper/gradle-wrapper.jar
 	@echo "==> downloading dependencies..."
 	@./gradlew deps --refresh-dependencies
 
@@ -15,21 +15,14 @@ test: build
 	@epmd &
 	@./gradlew checkAll -i --offline
 
-# target: tools - Bootstrap all build tools
-PHONY: tools
-tools: .asdf
-	@touch .envrc
-
 .PHONY: gradle/wrapper/gradle-wrapper.jar
 gradle/wrapper/gradle-wrapper.jar: .tool-versions
 	@gradle wrapper --gradle-version \
 		$$(cat .tool-versions | grep gradle | cut -d' ' -f2)
 
-
 # target: clean - Clean Java/Scala artifacts
 clean:
 	@./gradlew clean
-
 
 # target: clean-all - Clean up the project to start afresh
 clean-all:
@@ -38,20 +31,19 @@ clean-all:
 	@find . -name build | xargs rm -rf
 	@echo '==> keep in mind that some state is stored in ~/.gradle/caches/'
 
-
 .PHONY: clouseau1
 # target: clouseau1 - Start local inistance of clouseau1 node
-clouseau1: .asdf
+clouseau1:
 	@./gradlew run -Pnode=$@
 
 .PHONY: clouseau2
 # target: clouseau2 - Start local inistance of clouseau2 node
-clouseau2: .asdf
+clouseau2:
 	@./gradlew run -Pnode=$@
 
 .PHONY: clouseau3
 # target: clouseau3 - Start local inistance of clouseau3 node
-clouseau3: .asdf
+clouseau3:
 	@./gradlew run -Pnode=$@
 
 .PHONY: help
@@ -61,13 +53,6 @@ help:
 		| sed -e 's/^# target: //g' \
 		| sort \
 		| awk '{printf("    %-20s", $$1); $$1=$$2=""; print "-" $$0}'
-
-.asdf: .tool-versions
-	@cat $< | cut -d' ' -f 1 | xargs -I {} sh -c '\
-		asdf list {} > /dev/null 2>&1 \
-		|| asdf plugin-add {}'
-	@asdf install
-	@touch $@
 
 .PHONY: tree
 # target: tree - Print project source tree
---
+++ scripts/tools.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${SELF_DIR}/console.sh
+
+# call it as
+# result=( $(tools::requires) )
+tools::requires() {
+    local requirements=(
+        "grep"
+        "cat"
+        "sed"
+        "brew"
+    )
+    echo "${requirements[@]}"
+}
+
+# call it as
+# result=( $(tools::read_deps <file_name>) )
+tools::read_deps() {
+    local deps=()
+    while IFS= read -r line; do
+        deps+=(${line%'::'*})
+    done < <(cat "$1" | grep -v '\#')
+    echo "${deps[@]}"
+}
+
+tools::hint() {
+    line=$(cat $1 | grep "^$2::*")
+    echo "${line#*'::'}"
+}
+
+tools::has() {
+    case "$1" in
+        brew:*)
+            tools::in_brew ${1#"brew:"}
+            return
+            ;;
+        pkgutil:*)
+            tools::in_pkgutil ${1#"pkgutil:"}
+            return
+            ;;
+        *)
+            type "$1" &>/dev/null
+            return
+        ;;
+    esac
+}
+
+tools::in_brew() {
+    brew list | grep -Fx "$1" >>/dev/null 2>&1
+}
+
+tools::in_pkgutil() {
+    pkgutil --pkg-info="$1" >>/dev/null 2>&1
+}
+
+# This should be called as
+# tools=("brew" "coreutils")
+# misssing=( $(tools::missing "${tools[@]}") )
+tools::missing() {
+    local tools=("$@")
+    local missing=()
+    for tool in ${tools[@]}; do
+        if ! tools::has ${tool} ; then missing+=(${tool}); fi
+    done
+    echo "${missing[@]}"
+}
+
+# This should be called as
+# requirements=($(console::requires) $(tools::requires) "awk")
+# $(tools::verify "${requirements[@]}")
+function tools::verify() {
+    local requirements=("$@")
+    (console::info "checking requirements: '${requirements[@]}' ... ")
+    local missing=$(tools::missing "${requirements[@]}")
+    if [ -n "${missing}" ]; then
+        echo ""
+        (console::errorLn "The following mandatory commands are not available\n ${missing}")
+        exit 1
+    fi
+    (console::infoLn "OK")
+
+    local tools=( $(tools::read_deps ${SELF_DIR}/../.deps) )
+    (console::info "checking tools: '${tools[@]}' ... ")
+    local missing=$(tools::missing "${tools[@]}")
+    if [ -n "${missing}" ]; then
+        (
+            echo ""
+            (console::errorLn "Run the following commands to install missing dependencies")
+            for tool in "${missing[@]}"; do
+                hint=$(tools::hint "${SELF_DIR}/../.deps" "${tool}")
+                console::warn "    $(console::red)${hint}"
+            done
+            echo ""
+        )
+        exit 1
+    fi
+    (console::infoLn "OK")
+}
+
+function tools::bootstrap_topic() {
+    cat ${SELF_DIR}/bootstrap.md | awk "/##.*\`${1}\`:.*/,/---/ {print}" -
+}
+
+function tools::bootstrap() {
+    local shell=${1}
+    (console::info "detected shell '${shell}'")
+    case "${shell}" in
+        bash)
+            local config_file=~/.bash_profile
+            ;;
+        fish)
+            local config_file=~/.config/fish/config.fish
+            ;;
+        zsh)
+            local config_file=${ZDOTDIR:-~}/.zshrc
+            ;;
+        *)
+            console::warnLn "Unsupported shell ${shell}. You are on your own."
+            exit 1
+            ;;
+    esac
+    (console::info "checking asdf setup ... ")
+    if ! cat ${config_file} 2>/dev/null | grep 'asdf.fish' >>/dev/null 2>&1 ; then
+        console::warnLn "Action required!!!"
+        console::markdown "tools::bootstrap_topic ${shell}-asdf"
+        exit 1
+    fi
+    (console::infoLn "OK")
+    (console::info "checking direnv setup ... ")
+    if ! asdf current direnv >>/dev/null 2>&1; then
+        console::warnLn "Action required!!!"
+        console::markdown "tools::bootstrap_topic ${shell}-direnv"
+        exit 1
+    fi
+    if ! cat ~/.config/direnv/direnvrc 2>/dev/null | grep 'asdf direnv hook asdf' >>/dev/null 2>&1 ; then
+        console::warnLn "Action required!!!"
+        console::markdown "tools::bootstrap_topic direnv"
+        exit 1
+    fi
+    (console::infoLn "OK")
+    (console::infoLn "Congratulations!!! Your system is ready.")
+}
+
+if [[ "$(basename -- "$0")" == "tools.sh" ]]; then
+    echo "Don't run $0, source it" >&2
+    exit 1
+fi
