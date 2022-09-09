34b301ad25a2b1c2b1419dc1d5b669cc7edda3d4
jiahuili <Jiahui.Li@ibm.com>
2022-09-09 15:21:50 -0500

    Zio metrics JMX

--- experiments/src/test/scala/com/cloudant/ziose/experiments/HelloSpec.scala
+++ experiments/src/test/scala/com/cloudant/ziose/experiments/HelloSpec.scala
@@ -1,43 +1,48 @@
 package com.cloudant.ziose.experiments
 
-import zio.test.junit.ZTestJUnitRunner
-import zio.test.junit.JUnitRunnableSpec
 import org.junit.runner.RunWith
-import zio._
-import zio.test._
-import zio.test.Assertion._
+import zio.{ExitCode, LogLevel}
+import zio.test.Assertion.{anything, fails, isSubtype}
+import zio.test.TestAspect.withLiveClock
+import zio.test.{TestAspect, TestConsole, ZTestLogger, assertTrue, assertZIO}
+import zio.test.junit.{JUnitRunnableSpec, ZTestJUnitRunner}
 
 @RunWith(classOf[ZTestJUnitRunner])
 class HelloSpec extends JUnitRunnableSpec {
-  def spec = suite("Population Exercise")(
-    test("successful divide") {
-      for {
-        result <- Hello.divide(4, 2)
-      } yield {
-        assertTrue(result == 2)
+  val divisionSuite =
+    suite("Division Tests")(
+      test("successful divide") {
+        for {
+          result <- Hello.divide(4, 2)
+        } yield assertTrue(result == 2)
+      },
+      test("failed divide") {
+        assertZIO(Hello.divide(4, 0).exit)(
+          fails(isSubtype[ArithmeticException](anything))
+        )
       }
-    },
-    test("failed divide") {
-      assertZIO(Hello.divide(4, 0).exit)(
-        fails(isSubtype[ArithmeticException](anything))
-      )
-    },
-    test("test logger") {
-      for {
-        code   <- Hello.run
-        logs   <- ZTestLogger.logOutput
-        output <- TestConsole.output
-      } yield assertTrue(
-        output.nonEmpty &&
-          code == ExitCode.success &&
-          logs(0).logLevel == LogLevel.Info &&
-          logs(0).message() == "name: Ziose" &&
-          logs(1).logLevel == LogLevel.Error
-      )
-    }
-  )
+    )
+
+  val loggerSuite =
+    suite("Logger Tests")(
+      test("test logger") {
+        for {
+          code   <- Hello.run
+          logs   <- ZTestLogger.logOutput
+          output <- TestConsole.output
+        } yield assertTrue(
+          output.nonEmpty && code == ExitCode.success &&
+            (logs(0).logLevel == LogLevel.Info && logs(0).message() == "name: Ziose") &&
+            (logs(1).logLevel == LogLevel.Error && logs(1).message() == "n: 2") &&
+            (logs(2).logLevel == LogLevel.Warning && logs(2).message() == "Counter: 2") &&
+            (logs(3).logLevel == LogLevel.Warning && logs(3).message() == "Timer Count: 2")
+        )
+      } @@ withLiveClock @@ TestAspect.flaky @@ TestAspect.silent
+    )
+
+  def spec = suite("HelloTests")(divisionSuite, loggerSuite)
 }
 
 /*
-  gradle :experiments:test --tests HelloSpec
+gradle :experiments:test --tests HelloSpec
  */
