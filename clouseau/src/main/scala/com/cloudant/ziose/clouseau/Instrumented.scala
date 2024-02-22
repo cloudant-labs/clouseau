2b64ed0b19936e89ff121b1fb0729dc1c4900879
ILYA Khlopotov <iilyak@apache.org>
2024-02-22 14:24:06 -0800

    A stub on com.yammer.metrics.scala compatible API
    The main downside of this solution is that we place com.yammer.metrics.scala
    in the folder which belongs to `com.cloudant.ziose.clouseau`.
    
    This might break some IDEs and editors. For some editors (vscode) we can use
    the workaround:
    
    ```
    import _root_.com.yammer.metrics.scala._
    ```

---
+++ clouseau/src/main/scala/com/cloudant/ziose/clouseau/Instrumented.scala
@@ -0,0 +1,8 @@
+package com.cloudant.ziose.clouseau
+
+/**
+ * This is a trait used by clouseau. We don't use it at the moment.
+ * However we need it to be compatible with existing clouseau codebase.
+ * The mixin trait for creating a class which is instrumented with metrics.
+ */
+trait Instrumented {}
---
+++ clouseau/src/main/scala/com/cloudant/ziose/clouseau/metrics.scala
@@ -0,0 +1,41 @@
+/*
+com.yammer.metrics.scala compatible API
+
+This is a temporary approach until we get comfortable updating core clouseau classes
+*/
+
+package com.yammer.metrics.scala
+
+object metrics {
+  case class Timer(_name: String, metric: Unit) {
+    def time[A](f: => A): A = {
+      f
+    }
+  }
+
+  case class Counter(name: String, counter: Unit) {
+    def +=(v: Double): Counter = {
+    // Q: How to get correct runtime here the one which has the MeterRegistry service
+    //   Unsafe.unsafe { implicit unsafe =>
+    //     Runtime.default.unsafe.run(counter.update(1))
+    //   }
+      this.copy()
+    }
+    def -=(v: Double): Counter = {
+    //   Unsafe.unsafe { implicit unsafe =>
+    //     Runtime.default.unsafe.run(counter.update(1))
+    //   }
+      this.copy()
+    }
+  }
+
+  case class Gauge[T](name: String) {
+    def value(): T = ???
+  }
+
+  def timer(name: String): Timer = new Timer(name, ())
+  def counter(name: String): Counter = new Counter(name, ())
+  def gauge[T](name: String)(f: => T): Gauge[T] = new Gauge[T](name) {
+    override def value(): T = f
+  }
+}
