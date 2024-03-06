ea7294f71e12fb9c3aca4a8024654ae0a7d7ce9d
ILYA Khlopotov <iilyak@apache.org>
2024-03-06 13:19:04 -0800

    Add support for UpdateDocMsg into ClouseauTypeFactory

--- clouseau/src/main/scala/com/cloudant/ziose/clouseau/ClouseauTypeFactory.scala
+++ clouseau/src/main/scala/com/cloudant/ziose/clouseau/ClouseauTypeFactory.scala
@@ -1,7 +1,21 @@
 package com.cloudant.ziose.clouseau
 
+import scala.collection.JavaConverters._
+
 import com.cloudant.ziose.core.Codec._
 import com.cloudant.ziose.scalang.TypeFactory
+
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.StringField
+import org.apache.lucene.document.Field.Store
+import org.apache.lucene.facet.params.FacetIndexingParams
+import org.apache.lucene.facet.sortedset.SortedSetDocValuesFacetFields
+import org.apache.lucene.document.Field.Index
+import org.apache.lucene.document.Field.TermVector
+import org.apache.lucene.document.Field
+import org.apache.lucene.facet.taxonomy.CategoryPath
+import com.cloudant.ziose.core.Codec
+import com.cloudant.ziose.scalang.Pid
 
 class TermReader // we could just have a reference to mailbox here
 // but we should not remove abstraction
@@ -15,15 +29,17 @@ case class DeleteDocMsg(id: String)                               extends Clouse
 case class DiskSizeMsg(path: String)                              extends ClouseauMessage
 case class Group1Msg(query: String, field: String, refresh: Boolean, groupSort: Any, groupOffset: Int, groupLimit: Int)
     extends ClouseauMessage
-case class Group2Msg(options: Map[Symbol, Any])                 extends ClouseauMessage
-case class OpenIndexMsg(peer: EPid, path: String, options: Any) extends ClouseauMessage
-case class RenamePathMsg(dbName: String)                        extends ClouseauMessage
-case class SearchRequest(options: Map[Symbol, Any])             extends ClouseauMessage
-case class SetPurgeSeqMsg(seq: Long)                            extends ClouseauMessage
-case class SetUpdateSeqMsg(seq: Long)                           extends ClouseauMessage
+case class Group2Msg(options: Map[Symbol, Any])                extends ClouseauMessage
+case class OpenIndexMsg(peer: Pid, path: String, options: Any) extends ClouseauMessage
+case class RenamePathMsg(dbName: String)                       extends ClouseauMessage
+case class SearchRequest(options: Map[Symbol, Any])            extends ClouseauMessage
+case class SetPurgeSeqMsg(seq: Long)                           extends ClouseauMessage
+case class SetUpdateSeqMsg(seq: Long)                          extends ClouseauMessage
+case class UpdateDocMsg(id: String, doc: Document)             extends ClouseauMessage
 
 object ClouseauTypeFactory extends TypeFactory {
   type T = ClouseauMessage
+  val logger = LoggerFactory.getLogger("clouseau.tf")
 
   def parse(term: ETerm): Option[ClouseauMessage] = {
     term match {
@@ -53,6 +69,47 @@ object ClouseauTypeFactory extends TypeFactory {
         Some(Group2Msg(options.foldLeft(Map.empty[Symbol, Any]) { case (map, (k, v)) =>
           map + (k.asInstanceOf[EAtom].atom -> toScala(v))
         }))
+      case ETuple(EAtom(Symbol("update")), EString(id), fields: EList) => { // TODO verify maybe it should be EBinary(id)
+        var doc = new Document()
+        doc.add(new StringField("_id", id, Store.YES))
+        for (fieldE <- fields) {
+          val fieldS = Codec.toScala(fieldE)
+          fieldS match {
+            case (name: String, value: String, options: List[(String, Any) @unchecked]) =>
+              val map = {
+                options.collect { case t @ (key: String, value: Any) => t }.asInstanceOf[List[(String, Any)]].toMap
+              }
+              // case ETuple(List(EString(name), EString(value), EList(options))) =>
+              // val map = options.collect { case t @ ETuple(List(EString(key), value: ETerm)) => (key, Codec.toScala(value)) }.asInstanceOf[List[(String, Any)]].toMap
+              constructField(name, value, toStore(map), toIndex(map), toTermVector(map)) match {
+                case Some(field: Field) =>
+                  map.get("boost") match {
+                    case Some(boost: Number) =>
+                      field.setBoost(toFloat(1.2))
+                      'ok
+                    // make the match exhaustive
+                    case Some(_) =>
+                      'ok
+                    case None =>
+                      'ok
+                  }
+                  doc.add(field)
+                  if (isFacet(map) && value.nonEmpty) {
+                    val fp    = FacetIndexingParams.DEFAULT
+                    val delim = fp.getFacetDelimChar
+                    if (!name.contains(delim) && !value.contains(delim)) {
+                      val facets = new SortedSetDocValuesFacetFields(fp)
+                      facets.addFields(doc, List(new CategoryPath(name, value)).asJava)
+                    }
+                  }
+                case None =>
+                  'ok
+              }
+          }
+        }
+        val docId = doc.getField("_id").stringValue
+        Some(UpdateDocMsg(docId, doc))
+      }
       case ETuple(EAtom(Symbol("open")), peer, EString(path), options) =>
         Some(OpenIndexMsg(peer.asInstanceOf[EPid], path, options))
       case ETuple(EAtom(Symbol("rename")), EString(dbName)) =>
@@ -65,7 +122,94 @@ object ClouseauTypeFactory extends TypeFactory {
         Some(SetPurgeSeqMsg(seq.toLong))
       case ETuple(EAtom(Symbol("set_update_seq")), ELong(seq)) =>
         Some(SetUpdateSeqMsg(seq.toLong))
-      case _ => None
+      case other => None
+    }
+  }
+
+  private def constructField(name: String, value: String, store: Store, index: Index, tv: TermVector): Option[Field] = {
+    try {
+      Some(new Field(name, value, store, index, tv))
+    } catch {
+      case e: IllegalArgumentException =>
+        logger.error("Failed to construct field '%s' with reason '%s'".format(name, e.getMessage), e)
+        None
+      case e: NullPointerException =>
+        logger.error("Failed to construct field '%s' with reason '%s'".format(name, e.getMessage), e)
+        None
+    }
+  }
+
+  // These to* methods are stupid.
+
+  def toFloat(a: Any): Float = a match {
+    case v: java.lang.Double  => v.floatValue
+    case v: java.lang.Float   => v
+    case v: java.lang.Integer => v.floatValue
+    case v: java.lang.Long    => v.floatValue
+  }
+
+  def toDouble(a: Any): Option[Double] = a match {
+    case v: java.lang.Double  => Some(v)
+    case v: java.lang.Float   => Some(v.doubleValue)
+    case v: java.lang.Integer => Some(v.doubleValue)
+    case v: java.lang.Long    => Some(v.doubleValue)
+    case v: scala.math.BigInt => Some(v.doubleValue)
+    case _                    => None
+  }
+
+  def toLong(a: Any): Long = a match {
+    case v: java.lang.Integer => v.longValue
+    case v: java.lang.Long    => v
+  }
+
+  def toInteger(a: Any): Integer = a match {
+    case v: java.lang.Integer => v
+    case v: java.lang.Long    => v.intValue
+  }
+
+  def toStore(options: Map[String, Any]): Store = {
+    options.getOrElse("store", "no") match {
+      case true  => Store.YES
+      case false => Store.NO
+      case str: String =>
+        try {
+          Store.valueOf(str.toUpperCase)
+        } catch {
+          case _: IllegalArgumentException =>
+            Store.NO
+        }
+      case _ =>
+        Store.NO
+    }
+  }
+
+  def toIndex(options: Map[String, Any]): Index = {
+    options.getOrElse("index", "analyzed") match {
+      case true  => Index.ANALYZED
+      case false => Index.NO
+      case str: String =>
+        try {
+          Index.valueOf(str.toUpperCase)
+        } catch {
+          case _: IllegalArgumentException =>
+            Index.ANALYZED
+        }
+      case _ =>
+        Index.ANALYZED
+    }
+  }
+
+  def toTermVector(options: Map[String, Any]): TermVector = {
+    val termVector = options.getOrElse("termvector", "no").asInstanceOf[String]
+    TermVector.valueOf(termVector.toUpperCase)
+  }
+
+  def isFacet(options: Map[String, Any]) = {
+    options.get("facet") match {
+      case Some(bool: Boolean) =>
+        bool
+      case _ =>
+        false
     }
   }
 }
