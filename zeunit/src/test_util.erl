b036825413572112232eabf260b3dc9d7dd02949
Gabor Pali <gabor.pali@ibm.com>
2024-03-29 03:09:16 +0100

    Add unit tests for remote monitors

--- zeunit/src/test_util.erl
+++ zeunit/src/test_util.erl
@@ -1,14 +1,17 @@
 -module(test_util).
--export([setup/0, teardown/1]).
+-export([setup/0, teardown/1, spawn_echo/1]).
 
 -include("zeunit.hrl").
+
+spawn_echo(Name) ->
+    {ok, Pid} = gen_server:call({init, ?NodeZ}, {spawn, echo, Name}),
+    Pid.
 
 setup() ->
     {ok, _} = net_kernel:start(?NodeT, #{name_domain => longnames}),
     true = erlang:set_cookie(?NodeT, ?COOKIE),
     pong = util:check_ping(?NodeZ),
-    {ok, Pid} = gen_server:call({init, ?NodeZ}, {spawn, echo, echo}),
-    Pid.
+    spawn_echo(echo).
 
 teardown(Pid) ->
     exit(Pid, normal),
---
+++ zeunit/test/monitor_tests.erl
@@ -0,0 +1,59 @@
+-module(monitor_tests).
+-include_lib("zeunit/include/zeunit.hrl").
+
+monitor_test_() ->
+    {
+        "Test support for remote monitors",
+        {
+            setup,
+            fun test_util:setup/0,
+            fun test_util:teardown/1,
+            [
+                fun t_monitor_pid_noproc/0,
+                fun t_monitor_name_noproc/0,
+                fun t_monitor_pid/0,
+                fun t_monitor_name/0,
+                fun t_demonitor_pid/0,
+                fun t_demonitor_name/0
+            ]
+        }
+    }.
+
+t_monitor_pid_noproc() ->
+    InvalidPid = test_util:spawn_echo(echo_invalid),
+    exit(InvalidPid, normal),
+    {echo_invalid, ?NodeZ} ! {echo, self(), erlang:system_time(microsecond), 1},
+    {error, timeout} = util:receive_msg(util:seconds(1)),
+    Ref = monitor(process, InvalidPid),
+    ?assertMatch({'DOWN', Ref, process, InvalidPid, noproc}, util:receive_msg()).
+
+t_monitor_name_noproc() ->
+    Name = {non_existent, ?NodeZ},
+    monitor(process, Name),
+    ?assertMatch({'DOWN', _Ref, process, Name, noproc}, util:receive_msg()).
+
+t_monitor_pid() ->
+    Pid = test_util:spawn_echo(echo_monitor_pid),
+    Ref = monitor(process, Pid),
+    exit(Pid, reason),
+    ?assertMatch({'DOWN', Ref, process, Pid, reason}, util:receive_msg()).
+
+t_monitor_name() ->
+    Pid = test_util:spawn_echo(echo_monitor_name),
+    Ref = monitor(process, {echo_monitor_name, ?NodeZ}),
+    exit(Pid, reason),
+    ?assertMatch({'DOWN', Ref, process, Pid, reason}, util:receive_msg()).
+
+t_demonitor_pid() ->
+    Pid = test_util:spawn_echo(echo_demonitor_pid),
+    Ref = monitor(process, Pid),
+    demonitor(Ref),
+    exit(Pid, reason),
+    ?assertEqual({error, timeout}, util:receive_msg()).
+
+t_demonitor_name() ->
+    Pid = test_util:spawn_echo(echo_demonitor_name),
+    Ref = monitor(process, {echo_demonitor_name, ?NodeZ}),
+    demonitor(Ref),
+    exit(Pid, reason),
+    ?assertEqual({error, timeout}, util:receive_msg()).
