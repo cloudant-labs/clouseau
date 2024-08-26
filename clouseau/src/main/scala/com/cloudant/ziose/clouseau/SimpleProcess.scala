2c60ab50519d8c216834471b1392166d6bdda5fb
ILYA Khlopotov <iilyak@apache.org>
2024-08-26 11:40:54 -0700

    Call closure from onInit in SimpleProcess

--- clouseau/src/main/scala/com/cloudant/ziose/clouseau/SimpleProcess.scala
+++ clouseau/src/main/scala/com/cloudant/ziose/clouseau/SimpleProcess.scala
@@ -2,13 +2,25 @@ package com.cloudant.ziose.clouseau
 
 import com.cloudant.ziose.core
 import com.cloudant.ziose.scalang
+import zio._
+
+class SimpleProcess(fun: scalang.Process => Unit)(implicit adapter: scalang.Adapter[_, _])
+    extends scalang.Process()(adapter) {
+  override def onInit[PContext <: core.ProcessContext](ctx: PContext): ZIO[Any, Throwable, _ <: core.ActorResult] = {
+    ZIO.succeedBlocking {
+      fun(this)
+      core.ActorResult.Stop()
+    }
+  }
+  override def onTermination[PContext <: core.ProcessContext](reason: core.Codec.ETerm, ctx: PContext): UIO[Unit] = {
+    ZIO.logTrace(s"reason = ${reason}")
+  }
+}
 
 object SimpleProcess extends core.ActorConstructor[scalang.Process] {
   def make(node: scalang.SNode, fun: scalang.Process => Unit) = {
     def maker[PContext <: core.ProcessContext](process_context: PContext): scalang.Process = {
-      val proc = new scalang.Process()(scalang.Adapter(process_context, node, ClouseauTypeFactory))
-      fun(proc)
-      proc
+      new SimpleProcess(fun)(scalang.Adapter(process_context, node, ClouseauTypeFactory))
     }
     core
       .ActorBuilder()
