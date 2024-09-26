f66f8f40d26d986e1e0501f9f13e42e53c4537e9
ILYA Khlopotov <iilyak@apache.org>
2024-09-26 14:29:01 -0700

    Add test for encoding BytesRef

---
+++ clouseau/src/test/scala/com/cloudant/ziose/clouseau/ClouseauAdapterSpec.scala
@@ -0,0 +1,39 @@
+package com.cloudant.ziose.clouseau
+
+/*
+ * sbt -DZIOSE_TEST_DEBUG=true "testOnly com.cloudant.ziose.clouseau.ClouseauAdapterSpec"
+ */
+
+import com.cloudant.ziose.core.Codec._
+import com.cloudant.ziose.scalang.Adapter
+import com.cloudant.ziose.test.helpers.Utils
+import org.junit.runner.RunWith
+import zio._
+import zio.test._
+import zio.test.junit._
+import zio.ZIO._
+import org.apache.lucene.util.BytesRef
+import zio.test.Gen.alphaNumericString
+
+@RunWith(classOf[ZTestJUnitRunner])
+class ClouseauAdapterSpec extends JUnitRunnableSpec {
+  val logger      = Utils.logger
+  val environment = ZLayer.succeed(Clock.ClockLive) ++ ZLayer.succeed(Random.RandomLive) ++ logger
+  val adapter     = Adapter.mockAdapterWithFactory(ClouseauTypeFactory)
+
+  def spec = {
+    suite("Adapter term encoding")(
+      test("Correctly encode BytesRef into erlang binary")(
+        check(for { xs <- alphaNumericString } yield EBinary(xs)) { case term: EBinary =>
+          for {
+            _ <- logDebug(term.toString)
+            bytes = new BytesRef(term.asBytes)
+            decoded <- succeed(adapter.fromScala(bytes))
+          } yield assertTrue(
+            decoded == term
+          )
+        }
+      ).provideLayer(environment)
+    )
+  }
+}
