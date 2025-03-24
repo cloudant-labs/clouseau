73735466743ace1ac8670ba4068ae9aabaa4e9b3
Jiahui Li <jiahui.li@ibm.com>
2025-03-24 15:21:22 -0500

    Use Scaladoc instead of multi-line comments
    Only changed the `test` package.

--- test/src/test/scala/com/cloudant/ziose/test/helpers/LogHistory.scala
+++ test/src/test/scala/com/cloudant/ziose/test/helpers/LogHistory.scala
@@ -9,9 +9,9 @@ import com.cloudant.ziose.core.Address
 import com.cloudant.ziose.core.AddressableActor
 import zio.ZIO
 
-/*
+/**
  * This class implements a simple query language to find patterns in logs
- * ```scala
+ * {{{
  * for {
  *   output <- ZTestLogger.logOutput
  *   logHistory = LogHistory(output)
@@ -21,10 +21,11 @@ import zio.ZIO
  * )(containsShape { case (_, "onTermination", "EchoService") =>
  *   true
  * }) ?? "log should contain messages from 'EchoService.onTermination' callback")
- * ```
+ * }}}
  */
 class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
-  /*
+
+  /**
    * Returns new instance of `LogHistory` which contains only messages of given `LogLevel`.
    */
   def withLogLevel(level: LogLevel) = {
@@ -32,9 +33,10 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
       case (idx, entry) if entry.logLevel == level => (idx, entry)
     })
   }
-  /*
-   * Returns new instance of `LogHistory` which contains only messages which have
-   * given annotation and that annotation matches provided `PartialFunction[T, Boolean]` function.
+
+  /**
+   * Returns new instance of `LogHistory` which contains only messages which have given annotation and that annotation
+   * matches provided `PartialFunction[T, Boolean]` function.
    */
   def withContextAnnotation[T](annotation: LogAnnotation[T], pf: PartialFunction[T, Boolean]) = {
     new LogHistory(entries.collect {
@@ -42,9 +44,10 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
         (idx, entry)
     })
   }
-  /*
-   * Returns new instance of `LogHistory` which contains only messages which have
-   * given annotation and that annotation matches provided `T => Boolean` function.
+
+  /**
+   * Returns new instance of `LogHistory` which contains only messages which have given annotation and that annotation
+   * matches provided `T => Boolean` function.
    */
   def withContextAnnotation[T](annotation: LogAnnotation[T], condition: T => Boolean) = {
     new LogHistory(entries.collect {
@@ -52,43 +55,46 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
         (idx, entry)
     })
   }
-  /*
+
+  /**
    * Returns new instance of `LogHistory` which contains only messages from given `actorType: String`.
    */
   def withActor(actorType: String) = {
     withContextAnnotation(AddressableActor.actorTypeLogAnnotation, isEqual(actorType))
   }
-  /*
-   * Returns new instance of `LogHistory`
-   * which contains only messages of given `LogLevel` from given `actorType: String`.
+
+  /**
+   * Returns new instance of `LogHistory` which contains only messages of given `LogLevel` from given `actorType:
+   * String`.
    */
   def withActorLevel(actorType: String, level: LogLevel) = {
     withLogLevel(level) && withActor(actorType)
   }
-  /*
-   * Returns new instance of `LogHistory`
-   * which contains only messages of given `callbackName` from given `actorType: String`.
+
+  /**
+   * Returns new instance of `LogHistory` which contains only messages of given `callbackName` from given `actorType:
+   * String`.
    */
   def withActorCallback(actorType: String, callbackName: ActorCallback) = {
     withActor(actorType) && withActorCallbackName(callbackName)
   }
-  /*
-   * Returns new instance of `LogHistory`
-   * which contains only messages of given `LogLevel` from given `actorType: String`.
+
+  /**
+   * Returns new instance of `LogHistory` which contains only messages of given `LogLevel` from given `actorType:
+   * String`.
    */
   def withActorAddress(actorAddress: Address) = {
     withContextAnnotation(AddressableActor.addressLogAnnotation, isEqual(actorAddress))
   }
 
-  /*
-   * Returns new instance of `LogHistory`
-   * which contains only messages of given `callbackName`.
+  /**
+   * Returns new instance of `LogHistory` which contains only messages of given `callbackName`.
    */
   def withActorCallbackName(callbackName: ActorCallback) = {
     withContextAnnotation(AddressableActor.actorCallbackLogAnnotation, isEqual(callbackName))
   }
 
-  /*
+  /**
    * Computes the intersection of this history and another history.
    */
   def intersect(other: LogHistory) = {
@@ -99,7 +105,8 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
     }
     new LogHistory(resultMap.view.to(Chunk))
   }
-  /*
+
+  /**
    * Computes the union of this history and another history.
    */
   def union(other: LogHistory) = {
@@ -110,7 +117,8 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
     }
     new LogHistory(resultMap.view.to(Chunk))
   }
-  /*
+
+  /**
    * Computes the difference of this history and another history.
    */
   def diff(other: LogHistory) = {
@@ -121,31 +129,34 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
     }
     new LogHistory(resultMap.view.to(Chunk))
   }
-  /*
+
+  /**
    * Alias for `intersect`
    */
   def and(other: LogHistory) = intersect(other)
-  /*
+
+  /**
    * Alias for `union`
    */
   def or(other: LogHistory) = union(other)
-  /*
+
+  /**
    * Alias for `intersect`
    */
   def &&(other: LogHistory) = intersect(other)
-  /*
+
+  /**
    * Alias for `union`
    */
   def ||(other: LogHistory) = union(other)
 
-  /*
+  /**
    * Returns Chunk of indexed `LogEntry`
    */
   def asIndexedLogEntries: Chunk[(Int, LogEntry)] = entries
 
-  /*
-   * Returns Chunk of tuples `(index: Int, message: String)`
-   * which contain provided annotation.
+  /**
+   * Returns Chunk of tuples `(index: Int, message: String)` which contain provided annotation.
    */
   def asIndexedMessages: Chunk[(Int, String)] = entries.map { case (index, entry) =>
     (index, entry.message())
@@ -155,9 +166,8 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
     entry.context.getOrDefault(logContext).get(annotation)
   }
 
-  /*
-   * Returns Chunk of tuples `(index: Int, message: String, annotation: String)`
-   * which contain provided annotation.
+  /**
+   * Returns Chunk of tuples `(index: Int, message: String, annotation: String)` which contain provided annotation.
    */
   def asIndexedMessageAnnotationTuples[T](annotation: LogAnnotation[T]): Chunk[(Int, String, T)] = {
     entries.collect {
@@ -165,10 +175,10 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
         (idx, entry.message(), getAnnotation(entry, annotation).get)
     }
   }
-  /*
-   * Returns Chunk of tuples `(index: Int, message: String, annotations: List[Option[String]])`
-   * which contain provided annotations. The annotations are returned in the same order as they
-   * a listed in annotations argument.
+
+  /**
+   * Returns Chunk of tuples `(index: Int, message: String, annotations: List[Option[String]])` which contain provided
+   * annotations. The annotations are returned in the same order as they listed in annotations argument.
    */
   def asIndexedMessageAnnotationsTuples[T](
     annotations: List[LogAnnotation[T]]
@@ -177,9 +187,10 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
       (idx, entry.message(), annotations.map(annotation => getAnnotation(entry, annotation)))
     }
   }
-  /*
-   * Returns Chunk of tuples `(index: Int, message: String)`
-   * which contains only messages of given `LogLevel` from given `actorType: String`.
+
+  /**
+   * Returns Chunk of tuples `(index: Int, message: String)` which contains only messages of given `LogLevel` from given
+   * `actorType: String`.
    */
   def asActorLevelIndexedMessages(actorType: String, level: LogLevel) = {
     withActorLevel(actorType, level).asIndexedMessages
@@ -189,15 +200,16 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
     value == compareTo
   }
 
-  /*
+  /**
    * This debugging function can be used as
+   * {{{
    * for {
    *   output       <- ZTestLogger.logOutput
    *   logHistory = LogHistory(output)
    *   _ <- logHistory.debug(core.AddressableActor.actorTypeLogAnnotation)
    * } yield assert(....)
+   * }}}
    */
-
   def debug[T](annotation: LogAnnotation[T]) = {
     ZIO.succeed {
       asIndexedMessageAnnotationTuples(annotation).foreach { case (idx, message, callback) =>
@@ -206,6 +218,7 @@ class LogHistory private (val entries: Chunk[(Int, LogEntry)]) {
     }
   }
 }
+
 object LogHistory {
   def apply(entries: Chunk[LogEntry]) = new LogHistory(
     entries
