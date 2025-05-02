b84e719ec14ebe83964b87ff5e3fddb266b8d08e
ILYA Khlopotov <iilyak@apache.org>
2025-05-02 12:31:09 -0700

    Add test suite for LogConfiguration

--- clouseau/src/test/scala/com/cloudant/ziose/clouseau/ConfigSpec.scala
+++ clouseau/src/test/scala/com/cloudant/ziose/clouseau/ConfigSpec.scala
@@ -105,6 +105,73 @@ class ConfigSpec extends JUnitRunnableSpec {
 
     suite(s"configSuite for 'config.capacity.${key}'")(validTests ++ invalidTests)
   }
+
+  def logLevelFixture(level: String) = {
+    s"""
+       |logger {
+       |  level: ${level}
+       |}
+       |config: [
+       |  {
+       |    node: {
+       |      name: ziose1
+       |      domain: 127.0.0.1
+       |      cookie: cookie
+       |    }
+       |  }
+       |]
+       |""".stripMargin
+  }
+
+  def suiteForLogLevel(level: LogLevel): Spec[Any, Throwable] = {
+    def levelToString(level: LogLevel) = level.label.toLowerCase() match {
+      case "warn"    => "warning"
+      case "off"     => "none"
+      case lowerCase => lowerCase
+    }
+    val levelLowerCase   = levelToString(level)
+    val mixedCase        = levelLowerCase.capitalize
+    val trailingSpace    = levelLowerCase + " "
+    val leadingSpace     = " " + levelLowerCase
+    val levelWithTypo    = levelLowerCase + "typo"
+    val expectedLogLevel = s"LogLevel.${levelLowerCase.capitalize}"
+    suite(s"configSuite for 'logger.level' - '${levelLowerCase}'")(
+      test("Ensure we can parse log levels - mixed case")(
+        for {
+          config <- AppCfg.fromHoconString(logLevelFixture(mixedCase))
+        } yield assert(config.logger.level)(isSome(equalTo(level))) ?? s"Expected Some(${expectedLogLevel})"
+      ),
+      test("Ensure we can parse log levels - trailing space")(
+        for {
+          config <- AppCfg.fromHoconString(logLevelFixture(trailingSpace))
+        } yield assert(config.logger.level)(isSome(equalTo(level))) ?? s"Expected Some(${expectedLogLevel})"
+      ),
+      test("Ensure we can parse log levels - leading space")(
+        for {
+          config <- AppCfg.fromHoconString(logLevelFixture(leadingSpace))
+        } yield assert(config.logger.level)(isSome(equalTo(level))) ?? s"Expected Some(${expectedLogLevel})"
+      ),
+      test("Ensure we return 'InvalidData' error - typo")(
+        for {
+          error <- AppCfg.fromHoconString(logLevelFixture(levelWithTypo)).flip.exit
+        } yield assertTrue(
+          error.exists(_.isInstanceOf[Config.Error.InvalidData])
+        ) ?? "Expect error of type 'Config.Error.InvalidData'"
+          && assertTrue(
+            error.exists(_.asInstanceOf[Config.Error.InvalidData].path == Chunk("logger", "level"))
+          ) ?? "Expect error to be for 'logger.level' path"
+          && assertTrue(
+            error.exists(_.toString().contains(s"got '${levelWithTypo}'"))
+          ) ?? s"Expect error message to include provided value ('${levelWithTypo}')"
+          && assertTrue(
+            error.exists(
+              _.toString().contains("ALL|FATAL|ERROR|WARNING|INFO|DEBUG|TRACE|NONE")
+            )
+          ) ?? "Expect error message to contain hint of supported levels"
+      )
+    )
+  }
+
   def spec: Spec[Any, Throwable] = {
     suite("ConfigSpec")(
       suiteForCapacity("analyzer_exponent", capacity => capacity.analyzer_exponent),
@@ -112,7 +179,15 @@ class ConfigSpec extends JUnitRunnableSpec {
       suiteForCapacity("exchange_exponent", capacity => capacity.exchange_exponent),
       suiteForCapacity("index_exponent", capacity => capacity.index_exponent),
       suiteForCapacity("init_exponent", capacity => capacity.init_exponent),
-      suiteForCapacity("main_exponent", capacity => capacity.main_exponent)
+      suiteForCapacity("main_exponent", capacity => capacity.main_exponent),
+      suiteForLogLevel(LogLevel.All),
+      suiteForLogLevel(LogLevel.Fatal),
+      suiteForLogLevel(LogLevel.Error),
+      suiteForLogLevel(LogLevel.Warning),
+      suiteForLogLevel(LogLevel.Info),
+      suiteForLogLevel(LogLevel.Debug),
+      suiteForLogLevel(LogLevel.Trace),
+      suiteForLogLevel(LogLevel.None)
     )
   }
 }
