fab7fbc839195da3a63f90fce976d035ab2546ec
Gabor Pali <gabor.pali@ibm.com>
2026-07-06 21:15:56 +0200

    otp: make ping configurable for monitoring the CouchDB node
    It is not recommended to specify very long timeout when calling the
    `OtpNode#ping` method [1].  There is only a single attempt is made
    to connect to the remote node.  This is also a blocking call where
    it is not so useful to keep it waiting for a long time.
    
    Shorten the ping timeout to 1 second only by default but make it
    configurable by the user in case fine-tuning is required.
    
    [1] https://www.erlang.org/doc/apps/jinterface/assets/java/com/ericsson/otp/erlang/OtpNode.html#ping(java.lang.String,long)

--- clouseau/src/main/scala/com/cloudant/ziose/clouseau/Main.scala
+++ clouseau/src/main/scala/com/cloudant/ziose/clouseau/Main.scala
@@ -41,7 +41,11 @@ object Main extends ZIOAppDefault {
       runtime  <- ZIO.runtime[EngineWorker & Node & ActorFactory]
       otp_node <- ZIO.service[Node]
       remote_node = s"node${workerCfg.node.name.last}@${workerCfg.node.domain}"
-      _      <- otp_node.monitorRemoteNode(remote_node)
+      _ <- otp_node.monitorRemoteNode(
+        remote_node,
+        workerCfg.node.pingTimeoutResolved,
+        workerCfg.node.pingIntervalResolved
+      )
       worker <- ZIO.service[EngineWorker]
       logLevel = loggerCfg.level.getOrElse(LogLevel.Debug)
       node       <- ZIO.succeed(new ClouseauNode()(runtime, worker, metricsRegistry, logLevel))
--- otp/src/main/scala/com/cloudant/ziose/otp/OTPNodeConfig.scala
+++ otp/src/main/scala/com/cloudant/ziose/otp/OTPNodeConfig.scala
@@ -1,18 +1,31 @@
 package com.cloudant.ziose.otp
 
 import com.cloudant.ziose.macros.CheckEnv
-import zio.Config
+import zio.{Config, Duration, durationInt}
 import zio.config.magnolia.deriveConfig
 
-final case class OTPNodeConfig(name: String, domain: String, cookie: Option[String]) {
-  val cookieVal: String = cookie.getOrElse(OTPCookie.findOrGenerateCookie)
+final case class OTPNodeConfig(
+  name: String,
+  domain: String,
+  cookie: Option[String],
+  ping_timeout: Option[Duration],
+  ping_interval: Option[Duration]
+) {
+  val DEFAULT_PING_TIMEOUT: Duration  = 1.seconds
+  val DEFAULT_PING_INTERVAL: Duration = 60.seconds
+
+  val cookieResolved: String         = cookie.getOrElse(OTPCookie.findOrGenerateCookie)
+  val pingTimeoutResolved: Duration  = ping_timeout.getOrElse(DEFAULT_PING_TIMEOUT)
+  val pingIntervalResolved: Duration = ping_interval.getOrElse(DEFAULT_PING_INTERVAL)
 
   @CheckEnv(System.getProperty("env"))
   def toStringMacro: List[String] = List(
     s"${getClass.getSimpleName}",
     s"name=$name",
     s"domain=$domain",
-    s"cookie=****"
+    s"cookie=****",
+    s"ping_timeout=$pingTimeoutResolved",
+    s"ping_interval=$pingIntervalResolved"
   )
 }
 
